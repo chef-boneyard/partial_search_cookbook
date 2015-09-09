88be27fb3d98b502451049e33e029f0bad0c82f9
Tim Smith <tsmith84@gmail.com>
2015-09-09 12:25:27 -0700

    Rubocop fixes

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-name              "partial_search"
-maintainer        "Chef Software, Inc."
-maintainer_email  "cookbooks@chef.io"
-license           "Apache 2.0"
-description       "Provides experimental interface to partial search API in Chef Software Hosted Chef"
+name              'partial_search'
+maintainer        'Chef Software, Inc.'
+maintainer_email  'cookbooks@chef.io'
+license           'Apache 2.0'
+description       'Provides experimental interface to partial search API in Chef Software Hosted Chef'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.8"
+version           '1.0.8'
 source_url 'https://github.com/chef-cookbooks/partial_search' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-cookbooks/partial_search/issues' if respond_to?(:issues_url)
