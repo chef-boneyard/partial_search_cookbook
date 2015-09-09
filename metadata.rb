965bbfb524caf9733e3025f0036dd4027f8546b5
Tim Smith <tsmith84@gmail.com>
2015-09-09 12:21:18 -0700

    Add supermareket metadata

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license           "Apache 2.0"
 description       "Provides experimental interface to partial search API in Chef Software Hosted Chef"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.0.8"
+source_url 'https://github.com/chef-cookbooks/partial_search' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/partial_search/issues' if respond_to?(:issues_url)
