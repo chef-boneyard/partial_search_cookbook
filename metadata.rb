06f71dbf01600c6f1ad71acd94fd8d625a5a0204
Tim Smith <tsmith84@gmail.com>
2015-10-21 12:34:47 -0700

    Release 1.0.9

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email  'cookbooks@chef.io'
 license           'Apache 2.0'
 description       'Provides experimental interface to partial search API in Chef Software Hosted Chef for Chef-Client pre-12.0'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '1.0.8'
+version           '1.0.9'
 source_url 'https://github.com/chef-cookbooks/partial_search' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-cookbooks/partial_search/issues' if respond_to?(:issues_url)
